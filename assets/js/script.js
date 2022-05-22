dbc59f5973a688f2a5d5c32ff78af4fdf68ffc96
Davon <dkwebb95@gmail.com>
2022-05-22 02:01:12 -0400

    added rng individual function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 // Assignment code here
+const uppercaseEl = document.getElementById('upperCaseLetter');
+const lowercaseEl = document.getElementById('lowerCaseLetter');
+const numberEl = document.getElementById()
 
+function lowerCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function uppercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function number() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function specialCharacters() {
+  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+}
+function characterLimit() {
+  
+}
 
+console.log(specialCharacters());
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -8,10 +27,10 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
